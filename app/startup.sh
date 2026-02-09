45f4b42b6727a6306a9f8e9241e4df23bf30376c
jvanderzande <jvdzande@yahoo.com>
2026-02-09 11:01:59 +0100

    added some checks to startup.sh for hcix device availability

--- app/startup.sh
+++ app/startup.sh
@@ -3,8 +3,23 @@
 echo "- stopping conflicting processes"
 pkill hcitool btmgmt bluetoothd 2>/dev/null || true
 
-echo "- hciconfig hci0 up"
-hciconfig hci0 up
+sleep 2
+echo "- list available devices:hcitool dev"
+hcitool dev
+
+echo "- get first available devices with hcitool dev"
+dev=$(hcitool dev | awk '$1 ~ /^hci/ {print $1; exit}')
+if [ -n "$dev" ]; then
+   echo "Bluetooth device found: $dev"
+else
+   echo "No Bluetooth device found!"
+   echo "Retry in 10 seconds!"
+   sleep 10
+   exit 999
+fi
+
+echo "- hciconfig $dev up"
+hciconfig $dev up
 
 echo "- starting hcitool lescan"
 hcitool lescan --duplicates 1>/dev/null &
